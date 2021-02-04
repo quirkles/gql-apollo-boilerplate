2e73b9f0912c2941128d3b9200c3bfb8c5007801
quirkles <al.quirk@gmail.com>
2021-02-04 14:23:00 -0500

    remove huskyrc

--- husky.config.js
+++ husky.config.js
@@ -2,7 +2,7 @@ const tasks = (arr) => arr.join(' && ');
 
 module.exports = {
     hooks: {
-        'pre-commit': tasks(['nvm use', 'npm run lint', 'npm run test:unit']),
-        'pre-push': tasks(['nvm use', 'npm run lint', 'npm run test']),
+        'pre-commit': tasks(['npm run lint', 'npm run test:unit']),
+        'pre-push': tasks(['npm run lint', 'npm run test']),
     },
 };
